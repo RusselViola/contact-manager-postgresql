f157095a07eb03ca67076b78c70d086cf2e3e333
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-11-13 15:15:22 -0500

    adding old assignments

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require "sinatra/activerecord"
+require 'pry'
+
+require_relative 'models/contact'
+also_reload 'models/contact'
+
+get '/' do
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contacts' do
+  redirect '/'
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  erb :show
+end
